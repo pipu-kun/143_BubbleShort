4e61678ed5919e47d4ac8889ab0f9561c26b6145
pipu-kun <SideActivity@proton.me>
2026-03-05 03:48:50 +0000

    add Input

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,3 +5,19 @@ using namespace std;
 int arr[20];
 int n;
 
+void input(){
+    while (true) {
+        cout << "Enter number of element in array: ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "Array can have maximum 20 elements.\n";
+        }
+    }
+
+
+}
+
+
+
